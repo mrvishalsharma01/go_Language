f7eb6bd93fd4f9b9fe55d322d1850882c0d40235
vishalsharma <outreach.vishal@gmail.com>
2025-08-13 13:21:56 +0530

    learning go language

---
+++ simple-values/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(1 + 1) //integer
+
+	fmt.Println("Hello gloang") //sting
+
+	fmt.Println(true) //boolean
+
+	fmt.Println(false) //boolean
+
+	fmt.Println(10.5) //float
+
+	fmt.Println(7.0 / 3.0) //float
+
+}
---
+++ variables/variable.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var name string = "golang"
+
+	var name = "prince" // it automatically detects the type of variable
+
+	var age int = 30
+
+	//shorthand syntax
+
+	sirname := "princeSharma"
+
+	fmt.Println(sirname)
+
+	fmt.Println(age)
+
+	fmt.Println(name)
+
+
+
+
+
+
+
+
+
+
+
+
+}
