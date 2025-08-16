cc52c9bd38d89401182c9403221527acba70670b
vishalsharma <outreach.vishal@gmail.com>
2025-08-16 12:22:41 +0530

    updated variable

--- variables/variable.go
+++ variables/variable.go
@@ -5,10 +5,13 @@ import (
 )
 
 func main() {
+	
 	var student int = 12
 	var student2 string = "prince"
+	var age bool = true
 
 	fmt.Println(student)
 	fmt.Println(student2)
+	fmt.Println(age)
 
 }
